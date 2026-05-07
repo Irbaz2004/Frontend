e92b326bcba3b544e79a1cb6761c00d66cf28fcf
Irbaz2004 <irbazahmeds20@gmail.com>
2026-05-07 08:54:21 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -33,29 +33,30 @@ export default defineConfig({
           },
         ],
       },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
-              cacheableResponse: { statuses: [0, 200] },
-            },
-          },
-          {
-            urlPattern: /^http:\/\/localhost:5000\/api\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: { maxEntries: 50, maxAgeSeconds: 60 * 5 },
-              cacheableResponse: { statuses: [0, 200] },
-            },
-          },
-        ],
+     workbox: {
+  maximumFileSizeToCacheInBytes: 5000000,
+  globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-cache',
+        expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+        cacheableResponse: { statuses: [0, 200] },
       },
+    },
+    {
+      urlPattern: /^http:\/\/localhost:5000\/api\/.*/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'api-cache',
+        expiration: { maxEntries: 50, maxAgeSeconds: 60 * 5 },
+        cacheableResponse: { statuses: [0, 200] },
+      },
+    },
+  ],
+},
     }),
   ],
 })
